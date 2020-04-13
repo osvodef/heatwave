1ad02d1d6218bc04d44a62b2568b1d3cedc92c38
Alex Fedosov <osvodef@gmail.com>
2020-04-13 21:24:49 +0700

    tweak settings

--- src/settings.ts
+++ src/settings.ts
@@ -3,10 +3,10 @@ import { d3ColorScale } from './types';
 export const dataFile = 'nsk.csv';
 
 export const colorScale: d3ColorScale = 'interpolateMagma';
-export const paletteSize = 10;
-export const discrete = true;
+export const paletteSize = 12;
+export const discrete = false;
 export const reverse = false;
-export const gamma = 0.5;
+export const gamma = 0.25;
 
 export const tileSizePx = 256;
 export const opacity = 0.8;
